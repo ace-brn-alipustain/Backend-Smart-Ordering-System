e0e8fe602a359a7669c928556dea3d8ac7429f5f
ace.brn.ali <alipustainacebrian00433@gmail.com>
2026-03-23 13:07:40 +0800

    Initial commit

---
+++ src/main/java/com/ace_brn_alipustain/Smart_Ordering_System/SmartOrderingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ace_brn_alipustain.Smart_Ordering_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartOrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartOrderingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace_brn_alipustain/Smart_Ordering_System/SmartOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace_brn_alipustain.Smart_Ordering_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
